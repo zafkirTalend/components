17a0670aaef9753a66948449fc425ced099c0b72
Ivan Honchar <ivan.honchar@globallogic.com>
2016-07-26 15:38:10 +0300

    TDI-36946 Implemented DropboxDefinition

--- components-dropbox/src/main/java/org/talend/components/dropbox/DropboxDefinition.java
+++ components-dropbox/src/main/java/org/talend/components/dropbox/DropboxDefinition.java
@@ -1,68 +1,65 @@
-
+// ============================================================================
+//
+// Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
 package org.talend.components.dropbox;
 
-import org.talend.components.api.Constants;
 import org.talend.components.api.component.AbstractComponentDefinition;
-import org.talend.components.api.component.ComponentDefinition;
-import org.talend.components.api.component.ComponentImageType;
-import org.talend.components.api.component.InputComponentDefinition;
-import org.talend.components.api.component.runtime.Source;
-import org.talend.components.api.properties.ComponentProperties;
-
 import org.talend.daikon.properties.property.Property;
 
-import aQute.bnd.annotation.component.Component;
-
 /**
- * The DropboxDefinition acts as an entry point for all of services that a
- * component provides to integrate with the Studio (at design-time) and other
- * components (at run-time).
+ * Common definition class for Dropbox components. It acts as an entry point
+ * for all of services that a component provides to integrate with the Studio (at design-time)
+ * and other components (at run-time).
  */
-@Component(name = Constants.COMPONENT_BEAN_PREFIX + DropboxDefinition.COMPONENT_NAME, provide = ComponentDefinition.class)
-public class DropboxDefinition extends AbstractComponentDefinition implements InputComponentDefinition {
+public abstract class DropboxDefinition extends AbstractComponentDefinition {
 
-    public static final String COMPONENT_NAME = "Dropbox"; //$NON-NLS-1$
-
-    public DropboxDefinition() {
-        super(COMPONENT_NAME);
+    /**
+     * Constructor sets component name
+     * 
+     * @param componentName component name
+     */
+    public DropboxDefinition(String componentName) {
+        super(componentName);
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String[] getFamilies() {
-        return new String[] { "File/Input" }; //$NON-NLS-1$
+        return new String[] { "Cloud/Dropbox" }; //$NON-NLS-1$
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Property[] getReturnProperties() {
-        return new Property[] {};
+        return new Property[] { RETURN_ERROR_MESSAGE_PROP };
     }
 
-    @Override
-    public String getPngImagePath(ComponentImageType imageType) {
-        switch (imageType) {
-        case PALLETE_ICON_32X32:
-            return "fileReader_icon32.png"; //$NON-NLS-1$
-        default:
-            return "fileReader_icon32.png"; //$NON-NLS-1$
-        }
-    }
-
+    /**
+     * {@inheritDoc}
+     */
     public String getMavenGroupId() {
         return "org.talend.components";
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public String getMavenArtifactId() {
         return "components-dropbox";
     }
 
-    @Override
-    public Class<? extends ComponentProperties> getPropertyClass() {
-        return DropboxProperties.class;
-    }
-
-    @Override
-    public Source getRuntime() {
-        return new DropboxSource();
-    }
 }
