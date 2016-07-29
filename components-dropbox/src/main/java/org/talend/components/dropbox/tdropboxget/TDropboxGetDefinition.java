3d66d857efaf006fd5ca75e531ddde96cf392b56
Ivan Honchar <ivan.honchar@globallogic.com>
2016-07-29 12:49:04 +0300

    TDI-36946 Added TDropboxGetDefinition

---
+++ components-dropbox/src/main/java/org/talend/components/dropbox/tdropboxget/TDropboxGetDefinition.java
@@ -0,0 +1,59 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.components.dropbox.tdropboxget;
+
+import org.talend.components.api.Constants;
+import org.talend.components.api.component.ComponentDefinition;
+import org.talend.components.api.component.InputComponentDefinition;
+import org.talend.components.api.component.runtime.Source;
+import org.talend.components.api.properties.ComponentProperties;
+import org.talend.components.dropbox.DropboxDefinition;
+
+import aQute.bnd.annotation.component.Component;
+
+/**
+ * Dropbox get component definition
+ */
+@Component(name = Constants.COMPONENT_BEAN_PREFIX
+        + TDropboxGetDefinition.COMPONENT_NAME, provide = ComponentDefinition.class)
+public class TDropboxGetDefinition extends DropboxDefinition implements InputComponentDefinition {
+
+    /**
+     * Dropbox get component name
+     */
+    public static final String COMPONENT_NAME = "tDropboxGet";
+
+    /**
+     * Constructor sets component name
+     */
+    public TDropboxGetDefinition() {
+        super(COMPONENT_NAME);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Source getRuntime() {
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Class<? extends ComponentProperties> getPropertyClass() {
+        return null;
+    }
+
+}
---
+++ components-dropbox/src/test/java/org/talend/components/dropbox/tdropboxget/TDropboxGetDefinitionTest.java
@@ -0,0 +1,93 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.components.dropbox.tdropboxget;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.arrayContaining;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.talend.components.api.component.runtime.Source;
+import org.talend.components.dropbox.DropboxDefinition;
+
+/**
+ * Unit-tests for {@link TDropboxGetDefinition} class
+ */
+public class TDropboxGetDefinitionTest {
+
+    /**
+     * Check {@link TDropboxGetDefinition#getMavenGroupId()} returns "org.talend.components"
+     */
+    @Test
+    public void testGetMavenGroupId() {
+        DropboxDefinition definition = new TDropboxGetDefinition();
+        String mavenGroupId = definition.getMavenGroupId();
+        assertThat(mavenGroupId, equalTo("org.talend.components"));
+    }
+
+    /**
+     * Check {@link TDropboxGetDefinition#getMavenArtifactId()} returns "components-dropbox"
+     */
+    @Test
+    public void testGetMavenArtifactId() {
+        DropboxDefinition definition = new TDropboxGetDefinition();
+        String mavenArtifactId = definition.getMavenArtifactId();
+        assertThat(mavenArtifactId, equalTo("components-dropbox"));
+    }
+
+    /**
+     * Check {@link TDropboxGetDefinition#getFamilies()} returns string array, which contains "Cloud/Dropbox"
+     */
+    @Test
+    public void testGetFamilies() {
+        DropboxDefinition definition = new TDropboxGetDefinition();
+        String[] families = definition.getFamilies();
+        assertThat(families, arrayContaining("Cloud/Dropbox"));
+    }
+
+    /**
+     * Check {@link TDropboxGetDefinition#getName()} returns "tDropboxConnection"
+     */
+    @Test
+    public void testGetName() {
+        DropboxDefinition definition = new TDropboxGetDefinition();
+        String componentName = definition.getName();
+        assertEquals(componentName, "tDropboxGet");
+    }
+
+    /**
+     * Check {@link TDropboxGetDefinition#getPropertyClass()} returns class, which canonical name is
+     * "org.talend.components.jira.tjirainput.TDropboxConnectionProperties"
+     */
+    @Test
+    public void testGetPropertyClass() {
+        DropboxDefinition definition = new TDropboxGetDefinition();
+        Class<?> propertyClass = definition.getPropertyClass();
+        String canonicalName = propertyClass.getCanonicalName();
+        assertThat(canonicalName, equalTo("org.talend.components.dropbox.tdropboxconnection.TDropboxGetProperties"));
+    }
+
+    /**
+     * Check {@link TDropboxGetDefinition#getRuntime()} returns instance of {@link JiraSource}
+     */
+//    @Test
+//    public void testGetRuntime() {
+//        TDropboxGetDefinition definition = new TDropboxGetDefinition();
+//        Source source = definition.getRuntime();
+//        assertThat(source, is(instanceOf(DropboxSourceOrSink.class)));
+//    }
+
+}
