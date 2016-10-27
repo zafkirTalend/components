226aafe8e1ca22518713f2773cfacb8fee77583e
Ivan Honchar <ivan.honchar@globallogic.com>
2016-10-27 15:12:11 +0300

    chore(TCOMP-317): add routines sources and FileDelimited component processes

---
+++ performance-benchmark/src/main/java/org/talend/components/jobs/filedelimited/FileInputDelimitedSettings.java
@@ -0,0 +1,10 @@
+package org.talend.components.jobs.filedelimited;
+
+/**
+ * Sets Job process settings for FileInputDelimitedProcess
+ */
+public final class FileInputDelimitedSettings {
+    
+    public static final String FILE_NAME = "D:/TOSWorkspace/Performance/out.csv";
+
+}
