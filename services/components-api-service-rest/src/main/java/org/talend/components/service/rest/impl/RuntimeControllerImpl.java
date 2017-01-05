1836fcbba9d183073f216ff8ec0aa2016ba97bf5
Ryan Skraba <rskraba@talend.com>
2017-01-05 16:24:13 +0100

    fix(rest): get schema lazily when fetching sample.

--- services/components-api-service-rest/src/main/java/org/talend/components/service/rest/impl/RuntimeControllerImpl.java
+++ services/components-api-service-rest/src/main/java/org/talend/components/service/rest/impl/RuntimeControllerImpl.java
@@ -1,4 +1,4 @@
-//==============================================================================
+// ==============================================================================
 //
 // Copyright (C) 2006-2016 Talend Inc. - www.talend.com
 //
@@ -9,15 +9,19 @@
 // along with this program; if not, write to Talend SA
 // 9 rue Pages 92150 Suresnes, France
 //
-//==============================================================================
+// ==============================================================================
 
 package org.talend.components.service.rest.impl;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static java.lang.Integer.MAX_VALUE;
+import static java.util.Collections.emptyList;
+import static org.apache.commons.lang3.Validate.notNull;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.function.Function;
 
-import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericDatumWriter;
 import org.apache.avro.generic.IndexedRecord;
 import org.apache.avro.io.Encoder;
@@ -39,14 +43,10 @@ import org.talend.components.service.rest.dto.ValidationResultsDto;
 import org.talend.daikon.annotation.ServiceImplementation;
 import org.talend.daikon.exception.TalendRuntimeException;
 import org.talend.daikon.exception.error.CommonErrorCodes;
+import org.talend.daikon.java8.Consumer;
 import org.talend.daikon.properties.ValidationResult;
 import org.talend.daikon.runtime.RuntimeUtil;
 import org.talend.daikon.sandbox.SandboxedInstance;
-
-import static com.google.common.collect.Lists.newArrayList;
-import static java.lang.Integer.MAX_VALUE;
-import static java.util.Collections.emptyList;
-import static org.apache.commons.lang3.Validate.notNull;
 
 @ServiceImplementation
 @SuppressWarnings("unchecked")
@@ -59,20 +59,20 @@ public class RuntimeControllerImpl implements RuntimesController {
 
     @Override
     public ResponseEntity<ValidationResultsDto> validateDataStoreConnection(String dataStoreDefinitionName,
-                                                                            PropertiesDto propertiesContainer) {
-        final DatastoreDefinition<DatastoreProperties> definition = propertiesHelpers.getDataStoreDefinition(
-                dataStoreDefinitionName);
+            PropertiesDto propertiesContainer) {
+        final DatastoreDefinition<DatastoreProperties> definition = propertiesHelpers
+                .getDataStoreDefinition(dataStoreDefinitionName);
         notNull(definition, "Could not find data store definition of name %s", dataStoreDefinitionName);
         DatastoreProperties properties = propertiesHelpers.propertiesFromDto(propertiesContainer);
 
-        try (SandboxedInstance instance = RuntimeUtil.createRuntimeClass(definition.getRuntimeInfo(properties),
-                getClass().getClassLoader())) {
+        try (SandboxedInstance instance = RuntimeUtil.createRuntimeClass(definition.getRuntimeInfo(properties), getClass()
+                .getClassLoader())) {
             DatastoreRuntime<DatastoreProperties> datastoreRuntime = (DatastoreRuntime) instance.getInstance();
             datastoreRuntime.initialize(null, properties);
             Iterable<ValidationResult> healthChecks = datastoreRuntime.doHealthChecks(null);
 
-            ValidationResultsDto response = new ValidationResultsDto(
-                    healthChecks == null ? emptyList() : newArrayList(healthChecks));
+            ValidationResultsDto response = new ValidationResultsDto(healthChecks == null ? emptyList()
+                    : newArrayList(healthChecks));
             HttpStatus httpStatus = response.getStatus() == ValidationResult.Result.OK ? HttpStatus.OK : HttpStatus.BAD_REQUEST;
 
             return new ResponseEntity<>(response, httpStatus);
@@ -86,32 +86,32 @@ public class RuntimeControllerImpl implements RuntimesController {
 
     @Override
     public Void getDatasetData(String datasetDefinitionName, //
-                               PropertiesDto connectionInfo, //
-                               Integer from, //
-                               Integer limit, //
-                               OutputStream response) {
+            PropertiesDto connectionInfo, //
+            Integer from, //
+            Integer limit, //
+            OutputStream response) {
         return useDatasetRuntime(datasetDefinitionName, connectionInfo, new DatasetContentWriter(response, limit, true));
     }
 
     @Override
     public Void getDatasetDataAsBinary(String datasetDefinitionName, //
-                                       PropertiesDto connectionInfo, //
-                                       Integer from,  //
-                                       Integer limit,  //
-                                        OutputStream response) {
+            PropertiesDto connectionInfo, //
+            Integer from, //
+            Integer limit, //
+            OutputStream response) {
         return useDatasetRuntime(datasetDefinitionName, connectionInfo, new DatasetContentWriter(response, limit, false));
     }
 
     private <T> T useDatasetRuntime(String datasetDefinitionName, //
-                                    PropertiesDto formData, //
-                                    Function<DatasetRuntime<DatasetProperties<DatastoreProperties>>, T> consumer) {
+            PropertiesDto formData, //
+            Function<DatasetRuntime<DatasetProperties<DatastoreProperties>>, T> consumer) {
 
         // 1) get dataset properties from supplied data
         DatasetProperties datasetProperties = propertiesHelpers.propertiesFromDto(formData);
 
         // 2) Retrieve data set definition to be able to create the runtime
         final DatasetDefinition<DatasetProperties<DatastoreProperties>> datasetDefinition = //
-                propertiesHelpers.getDataSetDefinition(datasetDefinitionName);
+        propertiesHelpers.getDataSetDefinition(datasetDefinitionName);
 
         // 3) create the runtime
         try (SandboxedInstance instance = RuntimeUtil.createRuntimeClass(datasetDefinition.getRuntimeInfo(datasetProperties),
@@ -131,11 +131,12 @@ public class RuntimeControllerImpl implements RuntimesController {
         private final Integer limit;
 
         private final boolean json;
+
         private final OutputStream output;
 
         /**
          * @param limit the number of records to write
-         * @param json  true to write JSon, false for binary Avro
+         * @param json true to write JSon, false for binary Avro
          */
         DatasetContentWriter(OutputStream output, Integer limit, boolean json) {
             this.output = output;
@@ -145,18 +146,36 @@ public class RuntimeControllerImpl implements RuntimesController {
 
         @Override
         public Void apply(DatasetRuntime<DatasetProperties<DatastoreProperties>> dr) {
-            Schema schema = dr.getSchema();
-            GenericDatumWriter<IndexedRecord> writer = new GenericDatumWriter<>(schema);
+            // Schema schema = dr.getSchema();
+            //
             try {
-                Encoder encoder;
-                if (json) {
-                    encoder = EncoderFactory.get().jsonEncoder(schema, output);
-                } else {
-                    encoder = EncoderFactory.get().binaryEncoder(output, null);
-                }
-                dr.getSample(limit == null ? MAX_VALUE : limit, ir -> writeIndexedRecord(writer, encoder, ir));
-                encoder.flush();
-            } catch (IOException e) {
+                final Encoder[] encoder = { null };
+
+                dr.getSample(limit == null ? MAX_VALUE : limit, new Consumer<IndexedRecord>() {
+
+                    GenericDatumWriter<IndexedRecord> writer = null;
+
+                    @Override
+                    public void accept(IndexedRecord ir) {
+                        if (writer == null) {
+                            writer = new GenericDatumWriter<>(ir.getSchema());
+                            try {
+                                if (json) {
+                                    encoder[0] = EncoderFactory.get().jsonEncoder(ir.getSchema(), output);
+                                } else {
+                                    encoder[0] = EncoderFactory.get().binaryEncoder(output, null);
+                                }
+                            } catch (IOException ioe) {
+                                throw new RuntimeException(ioe);
+                            }
+
+                        }
+                        writeIndexedRecord(writer, encoder[0], ir);
+                    }
+                });
+                if (encoder[0] != null)
+                    encoder[0].flush();
+            } catch (RuntimeException | IOException e) {
                 log.error("Couldn't create Avro records JSon encoder.", e);
                 throw new TalendRuntimeException(CommonErrorCodes.UNEXPECTED_EXCEPTION, e);
             }
