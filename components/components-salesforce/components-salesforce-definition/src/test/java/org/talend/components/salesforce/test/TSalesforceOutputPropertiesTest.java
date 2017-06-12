cc5392f7caa7e30ef7e6e877ec1839ea27a2762e
Ihor Istomin <iistomin@talend.com>
2017-06-12 20:24:07 +0300

    chore(TDI-38989): Increase coverage for some definitions

--- components/components-salesforce/components-salesforce-definition/src/test/java/org/talend/components/salesforce/test/TSalesforceOutputPropertiesTest.java
+++ components/components-salesforce/components-salesforce-definition/src/test/java/org/talend/components/salesforce/test/TSalesforceOutputPropertiesTest.java
@@ -23,6 +23,7 @@ import static org.talend.components.salesforce.tsalesforceoutput.TSalesforceOutp
 
 import org.apache.avro.Schema;
 import org.apache.avro.SchemaBuilder;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ErrorCollector;
@@ -32,7 +33,9 @@ import org.talend.components.salesforce.tsalesforceoutput.TSalesforceOutputPrope
 import org.talend.daikon.properties.PropertiesDynamicMethodHelper;
 import org.talend.daikon.properties.presentation.Form;
 
-public class SalesforcePropertiesTest {
+public class TSalesforceOutputPropertiesTest {
+
+    private TSalesforceOutputProperties properties = new TSalesforceOutputProperties("foo");
 
     public static Schema DEFAULT_SCHEMA = SchemaBuilder.builder().record("Schema").fields() //
             .name("Id").type().stringType().noDefault() //
@@ -42,73 +45,83 @@ public class SalesforcePropertiesTest {
     @Rule
     public ErrorCollector errorCollector = new ErrorCollector();
 
+    @Before
+    public void setUp() {
+        properties = new TSalesforceOutputProperties("root");
+    }
+
     @Test
-    public void testSalesforceOutputProps() throws Throwable {
-        TSalesforceOutputProperties outputProps = new TSalesforceOutputProperties("foo");
-        outputProps = (TSalesforceOutputProperties) outputProps.init();
-        ComponentTestUtils.checkSerialize(outputProps, errorCollector);
+    public void testValuesAndLayout() throws Throwable {
+        properties.init();
 
         // check default
-        Form mainForm = outputProps.getForm(Form.MAIN);
+        Form mainForm = properties.getForm(Form.MAIN);
         assertEquals(Form.MAIN, mainForm.getName());
-        Form advancedForm = outputProps.getForm(Form.ADVANCED);
-        assertTrue(mainForm.getWidget(outputProps.outputAction.getName()).isVisible());
-        assertEquals(TSalesforceOutputProperties.OutputAction.INSERT, outputProps.outputAction.getValue());
-        Schema rejectSchema = outputProps.schemaReject.schema.getValue();
+        Form advancedForm = properties.getForm(Form.ADVANCED);
+        assertTrue(mainForm.getWidget(properties.outputAction.getName()).isVisible());
+        assertEquals(TSalesforceOutputProperties.OutputAction.INSERT, properties.outputAction.getValue());
+        Schema rejectSchema = properties.schemaReject.schema.getValue();
         assertNotNull(rejectSchema);
         assertEquals(0, rejectSchema.getFields().size());
-        Schema flowSchema = outputProps.schemaFlow.schema.getValue();
+        Schema flowSchema = properties.schemaFlow.schema.getValue();
         assertNotNull(flowSchema);
         assertEquals(0, flowSchema.getFields().size());
 
-        assertTrue(advancedForm.getWidget(outputProps.extendInsert.getName()).isVisible());
-        assertTrue(outputProps.extendInsert.getValue());
-        assertTrue(advancedForm.getWidget(outputProps.ceaseForError.getName()).isVisible());
-        assertTrue(outputProps.ceaseForError.getValue());
-        assertTrue(advancedForm.getWidget(outputProps.commitLevel.getName()).isVisible());
-        assertEquals(200, outputProps.commitLevel.getValue().intValue());
-        assertTrue(advancedForm.getWidget(outputProps.logFileName.getName()).isVisible());
-        assertNull(outputProps.logFileName.getValue());
+        assertTrue(advancedForm.getWidget(properties.extendInsert.getName()).isVisible());
+        assertTrue(properties.extendInsert.getValue());
+        assertTrue(advancedForm.getWidget(properties.ceaseForError.getName()).isVisible());
+        assertTrue(properties.ceaseForError.getValue());
+        assertTrue(advancedForm.getWidget(properties.commitLevel.getName()).isVisible());
+        assertEquals(200, properties.commitLevel.getValue().intValue());
+        assertTrue(advancedForm.getWidget(properties.logFileName.getName()).isVisible());
+        assertNull(properties.logFileName.getValue());
 
         // 1.After schema changed
-        outputProps.module.main.schema.setValue(DEFAULT_SCHEMA);
-        outputProps.module.schemaListener.afterSchema();
-        rejectSchema = outputProps.schemaReject.schema.getValue();
+        properties.module.main.schema.setValue(DEFAULT_SCHEMA);
+        properties.module.schemaListener.afterSchema();
+        rejectSchema = properties.schemaReject.schema.getValue();
         assertNotNull(rejectSchema);
         assertEquals(5, rejectSchema.getFields().size());
         assertEquals(4, rejectSchema.getField(FIELD_ERROR_MESSAGE).pos());
-        flowSchema = outputProps.schemaFlow.schema.getValue();
+        flowSchema = properties.schemaFlow.schema.getValue();
         assertNotNull(flowSchema);
         assertEquals(2, flowSchema.getFields().size());
 
         // 2.After extendInsert unchecked
-        outputProps.extendInsert.setValue(false);
-        assertTrue(advancedForm.getWidget(outputProps.extendInsert.getName()).isCallAfter());
-        PropertiesDynamicMethodHelper.afterProperty(outputProps, outputProps.extendInsert.getName());
-        assertTrue(advancedForm.getWidget(outputProps.retrieveInsertId.getName()).isVisible());
-        assertFalse(outputProps.retrieveInsertId.getValue());
+        properties.extendInsert.setValue(false);
+        assertTrue(advancedForm.getWidget(properties.extendInsert.getName()).isCallAfter());
+        PropertiesDynamicMethodHelper.afterProperty(properties, properties.extendInsert.getName());
+        assertTrue(advancedForm.getWidget(properties.retrieveInsertId.getName()).isVisible());
+        assertFalse(properties.retrieveInsertId.getValue());
 
         // 3.After retrieveInsertId checked
-        outputProps.retrieveInsertId.setValue(true);
-        assertTrue(advancedForm.getWidget(outputProps.retrieveInsertId.getName()).isCallAfter());
-        PropertiesDynamicMethodHelper.afterProperty(outputProps, outputProps.retrieveInsertId.getName());
+        properties.retrieveInsertId.setValue(true);
+        assertTrue(advancedForm.getWidget(properties.retrieveInsertId.getName()).isCallAfter());
+        PropertiesDynamicMethodHelper.afterProperty(properties, properties.retrieveInsertId.getName());
         // check schema changes
-        flowSchema = outputProps.schemaFlow.schema.getValue();
+        flowSchema = properties.schemaFlow.schema.getValue();
         assertNotNull(flowSchema);
         assertEquals(3, flowSchema.getFields().size());
         // Field "salesforce_id" is added
         assertEquals(2, flowSchema.getField(FIELD_SALESFORCE_ID).pos());
 
         // 3.After retrieveInsertId checked
-        outputProps.outputAction.setValue(SalesforceOutputProperties.OutputAction.UPSERT);
-        assertTrue(mainForm.getWidget(outputProps.outputAction.getName()).isCallAfter());
-        PropertiesDynamicMethodHelper.afterProperty(outputProps, outputProps.outputAction.getName());
-        flowSchema = outputProps.schemaFlow.schema.getValue();
+        properties.outputAction.setValue(SalesforceOutputProperties.OutputAction.UPSERT);
+        assertTrue(mainForm.getWidget(properties.outputAction.getName()).isCallAfter());
+        PropertiesDynamicMethodHelper.afterProperty(properties, properties.outputAction.getName());
+        flowSchema = properties.schemaFlow.schema.getValue();
         assertNotNull(flowSchema);
         assertEquals(4, flowSchema.getFields().size());
         // Field "salesforce_id" is added
         assertEquals(2, flowSchema.getField(FIELD_SALESFORCE_ID).pos());
         assertEquals(3, flowSchema.getField(FIELD_STATUS).pos());
 
+    }
+
+    @Test
+    public void testSerialize() throws Throwable {
+        properties.init();
+
+        ComponentTestUtils.checkSerialize(properties, errorCollector);
     }
 }
