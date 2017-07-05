42c5441980d3d497c7b505fbd4f791f9bc5933d5
Zakaria AFKIR <zafkir@talend.com>
2017-07-05 15:19:53 +0200

    feat(TFD-1513): Remove BeamJobBuilder interface implementation

--- components/components-processing/processing-runtime/src/main/java/org/talend/components/processing/runtime/normalize/NormalizeRuntime.java
+++ components/components-processing/processing-runtime/src/main/java/org/talend/components/processing/runtime/normalize/NormalizeRuntime.java
@@ -16,16 +16,13 @@ import org.apache.avro.generic.IndexedRecord;
 import org.apache.beam.sdk.transforms.PTransform;
 import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.PCollection;
-import org.apache.commons.lang3.StringUtils;
-import org.talend.components.adapter.beam.BeamJobBuilder;
-import org.talend.components.adapter.beam.BeamJobContext;
 import org.talend.components.api.component.runtime.RuntimableRuntime;
 import org.talend.components.api.container.RuntimeContainer;
 import org.talend.components.processing.normalize.NormalizeProperties;
 import org.talend.daikon.properties.ValidationResult;
 
 public class NormalizeRuntime extends PTransform<PCollection<IndexedRecord>, PCollection>
-        implements BeamJobBuilder, RuntimableRuntime<NormalizeProperties> {
+        implements RuntimableRuntime<NormalizeProperties> {
 
     private NormalizeProperties properties;
 
@@ -42,22 +39,5 @@ public class NormalizeRuntime extends PTransform<PCollection<IndexedRecord>, PCo
 
         PCollection outputCollection = inputPCollection.apply(properties.getName(), ParDo.of(doFn));
         return outputCollection;
-    }
-
-    @Override
-    public void build(BeamJobContext ctx) {
-        String mainLink = ctx.getLinkNameByPortName("input_" + properties.MAIN_CONNECTOR.getName());
-        if (!StringUtils.isEmpty(mainLink)) {
-            PCollection<IndexedRecord> mainPCollection = ctx.getPCollectionByLinkName(mainLink);
-            if (mainPCollection != null) {
-                String flowLink = ctx.getLinkNameByPortName("output_" + properties.FLOW_CONNECTOR.getName());
-
-                PCollection outputTuples = expand(mainPCollection);
-
-                if (!StringUtils.isEmpty(flowLink)) {
-                    ctx.putPCollectionByLinkName(flowLink, outputTuples);
-                }
-            }
-        }
     }
 }
